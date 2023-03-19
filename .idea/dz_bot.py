f4c2514fbd4195bb273ef48f777045ecf67d898d
Alexey <brazhnikovas_87@bk.ru>
2023-03-19 16:16:12 +0500

    Калькулятор

--- .idea/dz_bot.py
+++ .idea/dz_bot.py
@@ -1,7 +1,7 @@
 
 
 
-"""TOKEN = '5980477244:AAHuGfJGv_t130JTpZCD0Fcnn_YPnXPVmgE'"""
+"""мой токен = '5980477244:AAHuGfJGv_t130JTpZCD0Fcnn_YPnXPVmgE'"""
 
 import logging
 from aiogram import Bot, Dispatcher, types
@@ -9,36 +9,27 @@ from aiogram.types import message
 from aiogram.types import ParseMode
 from aiogram.utils import executor
 
-API_TOKEN = '5980477244:AAHuGfJGv_t130JTpZCD0Fcnn_YPnXPVmgE'
-
-
 logging.basicConfig(level=logging.INFO)
-bot = Bot(token=API_TOKEN)
+bot = Bot('5980477244:AAHuGfJGv_t130JTpZCD0Fcnn_YPnXPVmgE')
 dp = Dispatcher(bot)
 
 async def start_bot(_):
     print('Бот запущен')
+
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    await message.reply("Добро пожаловать! Этот бот поможет вам в использовании калькулятора. "
-                        "Чтобы использовать калькулятор, отправьте сообщение, содержащее математическое выражение.")
-
-
-@dp.message_handler(commands=['help'])
-async def send_help(message: types.Message):
-    await message.reply("Этот бот является калькулятором. Отправьте сообщение, содержащее математическое выражение, "
-                        "и бот вернет его результат. Все операции выполняются по порядку приоритетов.")
-
+    await message.reply('Это бот калькулятор\n'
+                        'Введите пример на сложение или вычитание:')
 
 @dp.message_handler(content_types=types.ContentType.TEXT)
 async def evaluate_expression(message: types.Message):
     expression = message.text
     try:
         result = eval(expression)
-        await message.reply(f"**Выражение:** `{expression}`\n**Результат:** `{result}`", parse_mode=ParseMode.MARKDOWN)
+        await message.reply(f'Вы в вели: `{expression}`\n Ответ: `{result}`', parse_mode=ParseMode.MARKDOWN)
     except (SyntaxError, ZeroDivisionError, TypeError, NameError):
-        await message.reply("Не удалось вычислить выражение. Проверьте правильность ввода.")
+        await message.reply('Не удалось посчитать.\n Введите пример на сложение или вычитание:')
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, skip_updates=True, on_startup=start_bot)
